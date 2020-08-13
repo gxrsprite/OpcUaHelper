768bdf986305002db8936b564b62b2d9b5ad1162
顾欣荣 <gxrsprite@163.com>
2020-08-13 12:21:51 +0800

    升级组件

---
+++ OpcUaSimpleClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpcUaHelper.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpcUaSimpleClient
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormBrowseServer());
+        }
+    }
+}
